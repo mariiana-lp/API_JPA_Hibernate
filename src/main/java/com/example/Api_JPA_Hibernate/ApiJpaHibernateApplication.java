73088fca66a5fe1f071eacc742cc4d6e58715ed9
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-02-21 16:06:45 -0500

    Inicio del proyecto

---
+++ src/main/java/com/example/Api_JPA_Hibernate/ApiJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.Api_JPA_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Api_JPA_Hibernate/ApiJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
